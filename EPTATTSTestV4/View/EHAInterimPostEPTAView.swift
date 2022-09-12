d37fdd7da2e3671c0058c2c847dd3826e5728bd1
Jeff <Jeff@truetosourcesound.com>
2022-09-12 09:57:57 -0600

    Mono Ear check templates TODO for interim EHAEPTA post test view

--- EPTATTSTestV4/View/EHAInterimPostEPTAView.swift
+++ EPTATTSTestV4/View/EHAInterimPostEPTAView.swift
@@ -26,6 +26,9 @@ struct EHAInterimPostEPTAView: View {
     @State var ehaSilentModeSettingString = ["No, Silent Mode Is Not Off. Please Repeat Silent Mode Setup", "Yes, Silent Mode Is Off"]
     @State var ehaSilentModeSettingIndex = Int()
     
+    @State var monoRightEarBetterExists = Bool()
+    @State var monoLeftEarBetterExists = Bool()
+    @State var monoEarTestingPan = Float()
     
     
     var body: some View {
@@ -37,6 +40,8 @@ struct EHAInterimPostEPTAView: View {
                 Text("Great Work!")
                     .foregroundColor(.white)
                 Text("Let's take a moment for a break")
+                    .foregroundColor(.white)
+                Text("MonoEarTestingPan: \(monoEarTestingPan)")
                     .foregroundColor(.white)
                 
                 HStack{
@@ -131,6 +136,9 @@ struct EHAInterimPostEPTAView: View {
             Task{
                 audioSessionModel.setAudioSession()
                 await checkInterimEHATestVolume()
+                await checkMonoRightTestLink()
+                await checkMonoLeftTestLink()
+                await monoEarBetterExists()
             }
         }
     }
@@ -189,6 +197,71 @@ struct EHAInterimPostEPTAView: View {
         await systemSettingsInterimStartEHAModel.saveSystemInterimStartingEHAToJSON()
         await systemSettingsInterimStartEHAModel.writeSystemInterimStartingEHAResultsToCSV()
         await systemSettingsInterimStartEHAModel.writeInputSystemInterimStartingEHAResultsToCSV()
+    }
+    
+    //TODO: Code to calculate delta at each EPTA frequency between ears. If gain delta is <= 2.5 dB (either at every frequency or in average, maybe with focus on 4kHz range, then allow user to select best ear test only for EHA Part 2. After completing this assessment, if applicable, generate a trigger csv file called. monoRightEarBetter.csv or monoLeftEarBetter.csv. Then check for each of these files and if one exists, that triggers the ability to test in that ear (Right or left) only for EHAP2. If neither file exists, then mono test is not available.
+    
+    func monoEarBetterExists() async {
+        if monoRightEarBetterExists == true && monoLeftEarBetterExists == false {
+            monoEarTestingPan = 1.0
+            print("monoRightEarBetter Test Link Exists")
+            print("monoLeftEarBetter Test Link DOES NOT Exist")
+            print("monoEarTestingPan: \(monoEarTestingPan)")
+        } else if  monoRightEarBetterExists == false && monoLeftEarBetterExists == true {
+            monoEarTestingPan = -1.0
+            print("monoLeftEarBetter Test Link Exists")
+            print("monoRightEarBetter Test Link DOES NOT Exist")
+            print("monoEarTestingPan: \(monoEarTestingPan)")
+        } else if  monoRightEarBetterExists == false && monoLeftEarBetterExists == false {
+            monoEarTestingPan = Float()
+            print("monoRightEarBetter Test Link DOES NOT Exist")
+            print("monoLeftEarBetter Test Link DOES NOT Exist")
+            print("monoEarTestingPan: \(monoEarTestingPan)")
+        } else if  monoRightEarBetterExists == true && monoLeftEarBetterExists == true {
+            monoEarTestingPan = Float()
+            print("!!!Critical Error, both Left and Right Mono Ear Link Files Exist")
+            print("monoRightEarBetter Test Link Exists")
+            print("monoLeftEarBetter Test Link Exists")
+            print("monoEarTestingPan: \(monoEarTestingPan)")
+        } else {
+            monoEarTestingPan = Float()
+            print("No mono ear link file exists ")
+            print("monoEarTestingPan: \(monoEarTestingPan)")
+        }
+    }
+    
+    func getTestLinkPath() async -> String {
+        let testLinkPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = testLinkPaths[0]
+        return documentsDirectory
+    }
+
+    func checkMonoRightTestLink() async {
+        let monoRightEarBetterName = ["monoRightEarBetter.csv"]
+        let fileManager = FileManager.default
+        let monoRightEarBetterPath = (await self.getTestLinkPath() as NSString).strings(byAppendingPaths: monoRightEarBetterName)
+        if fileManager.fileExists(atPath: monoRightEarBetterPath[0]) {
+            let monoRightEarBetterPath = URL(fileURLWithPath: monoRightEarBetterPath[0])
+            if monoRightEarBetterPath.isFileURL  {
+                monoRightEarBetterExists = true
+            } else {
+                print("monoRightEarBetter.csv Does Not Exist")
+            }
+        }
+    }
+    
+    func checkMonoLeftTestLink() async {
+        let monoLeftEarBetterName = ["monoLeftEarBetter.csv"]
+        let fileManager = FileManager.default
+        let monoLeftEarBetterPath = (await self.getTestLinkPath() as NSString).strings(byAppendingPaths: monoLeftEarBetterName)
+        if fileManager.fileExists(atPath: monoLeftEarBetterPath[0]) {
+            let monoLeftEarBetterPath = URL(fileURLWithPath: monoLeftEarBetterPath[0])
+            if monoLeftEarBetterPath.isFileURL  {
+                monoLeftEarBetterExists = true
+            } else {
+                print("monoLeftEarBetter.csv Does Not Exist")
+            }
+        }
     }
 }
 
